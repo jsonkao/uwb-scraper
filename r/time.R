f02e90b92aeeda03167bc618fecd8421dfbee6db
Jason Kao <jason.kao@columbia.edu>
2019-09-17 19:43:19 -0400

    r analysis

--- r/time.R
+++ r/time.R
@@ -23,16 +23,24 @@ courses %>%
   geom_col() +
   coord_flip()
 
-# Number of courses assigned a location by day
-tbaCalls <- fromJSON("../tba_calls.json") %>% 
-  mutate(date = as.Date(date))
-tbaCalls %>% 
+# Finds number of courses with NA or "To be announced" locations, then
+# takes differences to find out which were set to that.
+fromJSON("../tba_calls.json") %>% 
+  mutate(date = as.Date(date)) %>% 
   group_by(date) %>%
   filter(is.na(Location) | Location == "To be announced") %>% 
   count() %>% 
   ungroup() %>% 
   mutate(diff = n - lag(n, default = first(n))) %>% 
-  ggplot(aes(date, diff)) +
+  ggplot(aes(date, -diff)) +
   geom_col() +
   scale_x_date(date_breaks = "1 day", date_labels =  "%b %d") +
-  coord_flip()
+  labs(title = "Number of courses whose locations were set for the first time")
+
+# Number of classes that changed locations from the previous day
+fromJSON("../date_differences.json") %>% 
+  mutate(date = as.Date(date)) %>% 
+  ggplot(aes(date, differences)) +
+  geom_col() +
+  scale_x_date(date_breaks = "1 day", date_labels =  "%b %d") +
+  labs(title = "Number of courses whose locations were changed")
